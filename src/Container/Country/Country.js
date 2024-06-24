3378cc74b415a842a532a3024d1b5b60b4e43229
krupali2607 <kumbhanikrupali30@gmail.com>
2024-06-24 16:14:23 +0530

    24_06_24_setState

---
+++ src/Container/Country/Country.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+
+// rcc => react class based component
+export default class Country extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            countryName: 'India',
+            Population: '134 Billion'
+        }
+    }
+
+    onChange = () => {
+        this.setState(
+            {
+                countryName: 'UK',
+                Population: '100 Billion'
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>React Country Class Component</h1>
+                <p>{this.state.countryName}</p>
+                <p>{this.state.Population}</p>
+                <button onClick={this.onChange}>Change Value</button>
+            </div>
+        )
+    }
+}
---
+++ src/Container/Country/CountryFun/CountryFun.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+// rfc => React function based component
+const CountryFun = () => {
+  return (
+    <div>
+        <h1>React Function Based Component</h1>
+    </div>
+  )
+}
+
+export default CountryFun
