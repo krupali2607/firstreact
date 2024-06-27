057f74403b96e60cadd73996afd18e6030716f4f
krupali2607 <kumbhanikrupali30@gmail.com>
2024-06-27 17:15:48 +0530

    27_06_2024__cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import Country from './Container/Country/Country';
 import CountryFun from './Container/Country/CountryFun/CountryFun';
 import Course from './Container/Course/Course';
 import CourseFun from './Container/Course/CourseFun';
 import Fees from './Container/Fees/Fees';
 import FeesFunc from './Container/Fees/FeesFunc';
+import Medicines from './Container/Medicines/Medicines';
 import Timer from './Container/Timer/Timer';
 import TimerFunc from './Container/Timer/TimerFunc';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
       {/* <Country />
-      <CountryFun  />
-      <Timer /> 
-      <TimerFunc /> */}
-      <Course />
-      <CourseFun />
+      <CountryFun  /> */}
+      {/* <Timer />  */}
+      {/* <TimerFunc /> */}
+      <Medicines />
+      {/* <Course />
+      <CourseFun /> */}
 
     </>
   );
--- src/Container/Timer/TimerFunc.js
+++ src/Container/Timer/TimerFunc.js
@@ -1,21 +1,30 @@
 // rfs
 import React, { useState, useEffect } from 'react';
 
-
 function TimerFunc(props) {
-
+    //1.
     const [time, setTime] = useState(new Date());
+
 
     const tick = () => {
         setTime(new Date());
     }
 
+    //3. componentDidMount
     useEffect(() => {
         const timeRef = setInterval(tick, 1000);
-    });
+
+        // componentWillUnmount
+        return () => {
+            clearInterval(timeRef);
+        }
+
+        // componentDidUpdate[] In this bracket 
+    }, []);
 
     return (
         <div>
+            {/* 2. Print data */}
             <p>{time.toLocaleTimeString()}</p>
         </div>
     );
